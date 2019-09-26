aae99d6f553617e2b245fdfa83ea82e83b6ec02d
uppu15 <52970329+uppu15@users.noreply.github.com>
2019-09-26 10:54:25 -0700

    Different approach for my project.

---
+++ M_Web_database/MWeb1_2/MWeb1_2/Models/UserSetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MWeb1_2.Models
+{
+    public class UserSetting
+    {
+        public int userID { get; set; }
+        public decimal centerLat { get; set; }
+        public decimal centerLng { get; set; }
+        public int centerZoom { get; set; }
+        public string mapType { get; set; }
+        public int pinRadius { get; set; }
+        public bool GPS { get; set; }
+    }
+}
