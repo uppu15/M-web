801086ae5827cbf42e87e65f78f16493fb860f00
Jehyun Park <uppu1507@gmail.com>
2019-10-09 22:08:03 -0700

    Play with Newtonsoft.Json

---
+++ Prototype/MWeb_test/MWeb_test/Models/MarkerJSON.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Newtonsoft.Json;
+
+//namespace MWeb_test.Models
+//{
+//    public class MarkerJSON
+//    {
+//        public string DataTableTOJSONWithJSONNet(Markers markers)
+//        {
+//            string JSONString = string.Empty;
+//            JSONString = JsonConvert.SerializeObject(markers);
+//            return JSONString;
+//        }
+//    }
+//}
---
+++ Prototype/MWeb_test/MWeb_test/Models/ModelView/MarkerCommentRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MWeb_test.Models.ModelView
+{
+    public class MarkerCommentRepository
+    {
+        public Markers markers { get; set; }
+        public Comments comments { get; set; }
+    }
+
+    public class DataTablestoJson
+    {
+        MarkerCommentRepository MarComRepo = new MarkerCommentRepository
+        {
+
+        };
+
+        string MarkerCommentJson = JsonConvert.SerializeObject(MarComRepo);
+    }
+}
